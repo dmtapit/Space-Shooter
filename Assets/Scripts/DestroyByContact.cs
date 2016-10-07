fce5ba1d6d9287da7f70ab87f23fc8f2d14cb6b7
Dean Tapit <dmtapit@cpp.edu>
2016-10-06 18:22:10 -0700

    Commit Space Shooter repo from lappy

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	private GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) {
+			gameController = gameControllerObject.GetComponent<GameController> ();
+		}
+		if (gameController == null) {
+			Debug.Log("Cannot find 'GameController' script");
+		}
+			
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log (other.name);
+		//if (other.tag == "Boundary" || other.tag == "Enemy") 
+		if (other.CompareTag ("Boundary") || other.CompareTag ("Enemy"))
+		{
+			return;
+		}
+
+		if (explosion != null)
+		{
+			Instantiate (explosion, transform.position, transform.rotation);
+		}
+
+		//if (other.tag == "Player") 
+		if (other.CompareTag ("Player"))
+		{
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+
+		gameController.AddScore (scoreValue);
+		Destroy (other.gameObject); // Marks object to be destroyed until end of frame
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable] // Make it serializable and visible in the Inspector to Unity
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour 
+{
+	private Rigidbody rb; // Unity 5, no more shorthand helper references
+	private AudioSource audioSource;
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+
+	void Update ()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			//GameObject clone = //used to create as a reference
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation); //as GameObject;
+			audioSource.Play();
+		}
+	}
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> (); // Must use GetComponent, Unity 5
+		audioSource = GetComponent<AudioSource> ();
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		rb.position = new Vector3 
+		(
+				Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+				0.0f, 
+				Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
